aac84aeb8b63fe74d7e3bf6c2c8c30a60bc8d3eb
imarkwick <izzymarkwick@gmail.com>
2014-11-23 18:44:35 +0000

    green tests

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -11,6 +11,14 @@ describe ('Javabuzz',function() {
 		it('divisible by 3', function() {
 			expect(javabuzz.isDivisibleByThree(3)).toEqual(true);
 		});
+
+		it('divisible by 5', function() {
+			expect(javabuzz.isDivisibleByFive(5)).toEqual(true);
+		});
+
+		it('divisible by 15', function() {
+			expect(javabuzz.isDivisibleByFifteen(15)).toEqual(true);
+		});
 	});
 
 	describe('knows when a number is NOT', function() {
--- src/Javabuzz.js
+++ src/Javabuzz.js
@@ -1,6 +1,17 @@
 function Javabuzz() {};
 
+// Javabuzz.prototype._isDivisibleBy = function(number, divisor) {
+// 	return (number % divisor === 0);
+// };
+
 Javabuzz.prototype.isDivisibleByThree = function(number) {
-	return (number % 3 === 0)
+	return (number % 3 === 0);
 };
 
+Javabuzz.prototype.isDivisibleByFive = function(number) {
+	return (number % 5 === 0);
+};
+
+Javabuzz.prototype.isDivisibleByFifteen = function(number) {
+	return (number % 15 === 0);
+};
