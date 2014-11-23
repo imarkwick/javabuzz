1feb1c10e4c9325a12bef1ce7f5107ae231703ae
imarkwick <izzymarkwick@gmail.com>
2014-11-23 19:10:17 +0000

    javabuzz game complete

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -36,4 +36,24 @@ describe ('Javabuzz',function() {
 		});
 	});
 
+	describe('when playing, says', function() {
+
+		it('"Java" when a number is divisible by 3', function() {
+			expect(javabuzz.says(3)).toEqual("Java");
+		});
+
+		it('"Buzz" when a number is divisible by 5', function() {
+			expect(javabuzz.says(5)).toEqual("Buzz");
+		});
+
+		it('"Javabuzz" when a number is divisible by 15', function() {
+			expect(javabuzz.says(15)).toEqual("Javabuzz");
+		});
+
+		it('returns the number if none of above', function() {
+			expect(javabuzz.says(1)).toEqual(1);
+		});
+
+	});
+
 });
--- src/Javabuzz.js
+++ src/Javabuzz.js
@@ -16,6 +16,13 @@ Javabuzz.prototype.isDivisibleByFifteen = function(number) {
 	return this._isDivisibleBy(number, 15);
 };
 
+Javabuzz.prototype.says = function(number) {
+	if (this.isDivisibleByFifteen(number)) return "Javabuzz";
+	if (this.isDivisibleByThree(number)) return "Java";
+	if (this.isDivisibleByFive(number)) return "Buzz";
+	return number;
+};
+
 
 // Javabuzz.prototype.isDivisibleByThree = function(number) {
 // 	return (number % 3 === 0);
